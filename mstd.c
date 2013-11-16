e34a8f582bab063335a0a78511b7dd72db1b7f7a
James Wilcox <wilcoxjay@gmail.com>
2013-11-15 21:44:34 -0800

    a lot of 1d stuff

--- mstd.c
+++ mstd.c
@@ -43,7 +43,7 @@ int bitset_count(char* s, int N) {
   return count;
 }
 
-int is_mstd(char* s, int N, char* sum, char* diff) {
+int is_mstd1d(char* s, int N, char* sum, char* diff) {
   int i, j;
   for (i = 0; i < N; i++) {
     if (!bitset_get(s, i, N)) continue;
@@ -51,8 +51,8 @@ int is_mstd(char* s, int N, char* sum, char* diff) {
       if (!bitset_get(s, j, N)) continue;
       bitset_set(sum, i+j, 2*N);
       int d = i - j;
-      bitset_set(diff, d + N, 2*N);
-      bitset_set(diff, N - d, 2*N);
+      bitset_set(diff, d + N - 1, 2*N);
+      bitset_set(diff, N - d - 1, 2*N);
     }
   }
 
@@ -92,7 +92,7 @@ void print_set2d(char* s, int rows, int cols) {
 
 int is_mstd2d(char* s, int rows, int cols, char* sum, char* diff) {
   int N = rows * cols;
-  
+
   int i, j;
   for (i = 0; i < N; i++) {
     if (!bitset_get(s, i, N)) continue;
@@ -149,7 +149,42 @@ void check(long long x, int R, int C) {
   printf(" %d\n\n\n\n", bitset_count(diff, 4*N));
 }
 
-void parallel_exhaustive_search(int R, int C) {
+void parallel_exhaustive_search1d(int N) {
+  long long max = 1LL << N;
+#pragma omp parallel
+  {
+    int tid = omp_get_thread_num();
+    #pragma omp single
+    {
+      printf("running on %d threads\n", omp_get_num_threads());
+    }
+
+    long long scratch[4];
+    char* sum = (char*)scratch;
+    char* diff = (char*)(scratch + 2);
+    long long i;
+#pragma omp for schedule(guided), nowait
+    for (i = 0; i < max; i++) {
+      memset(scratch, 0, 4*sizeof(long long));
+
+      char* s = (char*)&i;
+      if (is_mstd1d(s, N, sum, diff)) {
+#pragma omp critical
+	{
+	  print_set(s, N);
+	  printf(" %d\n", bitset_count(s, N));
+	  print_set(sum, 2*N);
+	  printf(" %d\n", bitset_count(sum, 2*N));
+	  print_set(diff, 2*N);
+	  printf(" %d\n", bitset_count(diff, 2*N));
+	  printf("tid %d, seed %lld\n\n", tid, i);
+	}
+      }
+    }
+  }
+}
+
+void parallel_exhaustive_search2d(int R, int C) {
   int N = R * C;
   long long max = 1LL << N;
 #pragma omp parallel
@@ -170,10 +205,10 @@ void parallel_exhaustive_search(int R, int C) {
     char* diff = (char*)(scratch+4);
     long long i;
 
-#pragma omp for schedule(guided)
+#pragma omp for schedule(guided), nowait
     for (i = 0; i < max; i++) {
       memset(scratch, 0, 8*sizeof(long long));
-    
+
       char* s = (char*) &i;
       if (is_mstd2d(s, R, C, sum, diff)) {
 #pragma omp critical
@@ -196,12 +231,129 @@ void parallel_exhaustive_search(int R, int C) {
   }
 }
 
-void do_stack(int tid, int R, int C, int N, long long s, char* p_sum, char* p_diff, int i) {
+void do_stack1d(int tid, int N, long long s, long long r, char* p_sum, char* p_diff, int i) {
+  char* t = (char*)&s;
+  if (i >= N) {
+    //printf("do_stack1d: N=%d, i=%d, s=%lld, r=%lld\n", N, i, s, r);
+    if (bitset_count(p_sum, 2*N) > bitset_count(p_diff, 2*N)) {
+#pragma omp critical
+      {
+	print_set(t, N);
+	printf(" %d\n", bitset_count(t, N));
+	print_set((char*)&r, N);
+	printf(" %d\n", bitset_count((char*)&r, N));
+	print_set(p_sum, 2*N);
+	printf(" %d\n", bitset_count(p_sum, 2*N));
+	print_set(p_diff, 2*N);
+	printf(" %d\n", bitset_count(p_diff, 2*N));
+
+	printf("**tid %d, seed: %lld\n\n", tid, s);
+      }
+    }
+  } else {
+    long long scratch[4];
+    char *sum = (char*) scratch;
+    char *diff = (char*) (scratch + 2);
+    memcpy(sum, p_sum, 2*sizeof(long long));
+    memcpy(diff, p_diff, 2*sizeof(long long));
+
+    if (i < N - 1) {
+      do_stack1d(tid, N, s, r, sum, diff, i+1);
+    }
+
+    s |= 1LL << i;
+    ////
+    r |= 1LL << (N - i - 1);
+
+    scratch[0] |= s << i;
+    if (i > 0) {
+      scratch[1] |= s >> (64 - i);
+    }
+
+    scratch[2] |= s << (N - i - 1);
+    if (N - i - 1 > 0) {
+      scratch[3] |= s >> (64 - N + i + 1);
+    }
+
+    scratch[2] |= r << i;
+    if (i > 0) {
+      scratch[3] |= r >> (64 - i);
+    }
+    //// the following is the straightforward version of the above:
+    // int j;
+    // for (j = 0; j < N; j++) {
+    //   if (!bitset_get(t, j, N)) continue;
+    //
+    //   int s = i + j;
+    //   int d1 = i - j + N;
+    //   int d2 = j - i + N;
+    //
+    //   bitset_set(sum, s, 2*N);
+    //   bitset_set(diff, d1, 2*N);
+    //   bitset_set(diff, d2, 2*N);
+    // }
+    ////
+
+    do_stack1d(tid, N, s, r, sum, diff, i+1);
+  }
+}
+
+void stack_search1d(int N) {
+  long long scratch[4];
+  memset(scratch, 0, 4 * sizeof(long long));
+
+  do_stack1d(0, N, 0, 0, (char*)scratch, (char*)(scratch + 2), 0);
+}
+
+long long flip_about(int N, long long x) {
+  long long result = 0;
+  int i;
+  for (i = 0; i < N; i++) {
+    result |= ((x >> i) & 1) << (N - i - 1);
+  }
+  return result;
+}
+
+void parallel_stack_search1d(int N) {
+
+#pragma omp parallel
+  {
+    int tid = omp_get_thread_num();
+
+    long long scratch[4];
+
+#pragma omp critical
+    printf("tid %d starting up.\n", tid);
+
+    long long max = 1LL << (N/3);
+#pragma omp single
+    printf("using mask of size %d\n", N/3);
+
+    long long i;
+#pragma omp for schedule(guided), nowait
+    for (i = 1; i < max; i += 2) {
+#pragma omp critical
+      printf("tid %d beginning mask %lld\n", tid, i);
+      memset(scratch, 0, 4 * sizeof(long long));
+
+      is_mstd1d((char*)&i, N, (char*)scratch, (char*)(scratch + 2));
+
+      do_stack1d(tid, N, i, flip_about(N, i), (char*)scratch, (char*)(scratch + 2), N/3);
+
+#pragma omp critical
+      printf("tid %d ending mask %lld\n", tid, i);
+    }
+#pragma omp critical
+    printf("tid %d done.\n", tid);
+  }
+}
+
+void do_stack2d(int tid, int R, int C, int N, long long s, char* p_sum, char* p_diff, int i) {
   if (i >= N) {
     if (bitset_count(p_sum, 4*N) > bitset_count(p_diff, 4*N)) {
 #pragma omp critical
       {
-	printf("do_stack: N=%d, i=%d, s=%lld\n", N, i, s);
+	printf("do_stack2d: N=%d, i=%d, s=%lld\n", N, i, s);
 	print_set2d((char*)&s, R, C);
 	printf(" %d\n", bitset_count((char*)&s, N));
 	print_set2d(p_sum, 2*R, 2*C);
@@ -217,7 +369,7 @@ void do_stack(int tid, int R, int C, int N, long long s, char* p_sum, char* p_di
     memcpy(scratch + 4, p_diff, 4 * sizeof(long long));
     char* sum = (char*)scratch;
     char* diff = (char*)(scratch + 4);
-    do_stack(tid, R, C, N, s, sum, diff, i+1);
+    do_stack2d(tid, R, C, N, s, sum, diff, i+1);
 
     int ir = i / C;
     int ic = i % C;
@@ -236,27 +388,27 @@ void do_stack(int tid, int R, int C, int N, long long s, char* p_sum, char* p_di
       int dc1 = ic - jc + C;
       int dr2 = jr - ir + R;
       int dc2 = jc - ic + C;
-      
+
       bitset_set(sum, sr * 2 * C + sc, 4*N);
       bitset_set(diff, dr1 * 2 * C + dc1, 4*N);
       bitset_set(diff, dr2 * 2 * C + dc2, 4*N);
     }
 
-    do_stack(tid, R, C, N, s, sum, diff, i+1);
-    
-  }    
+    do_stack2d(tid, R, C, N, s, sum, diff, i+1);
+
+  }
 }
 
-void stack_search(int R, int C) {
+void stack_search2d(int R, int C) {
   int N = R * C;
 
   long long scratch[8];
   memset(scratch, 0, 8 * sizeof(long long));
 
-  do_stack(0, R, C, N, 0, (char*)scratch, (char*)(scratch + 4), 0);
+  do_stack2d(0, R, C, N, 0, (char*)scratch, (char*)(scratch + 4), 0);
 }
 
-void parallel_stack_search(int R, int C) {
+void parallel_stack_search2d(int R, int C) {
   int N = R * C;
 
 #pragma omp parallel
@@ -267,7 +419,7 @@ void parallel_stack_search(int R, int C) {
 
     long long scratch[8];
     char* sum = (char*)scratch;
-    char* diff = (char*)(scratch+4); 
+    char* diff = (char*)(scratch+4);
 
     long long max = 1LL << (N/3);
 #pragma omp single
@@ -282,8 +434,8 @@ void parallel_stack_search(int R, int C) {
       // compute the sum and difference sets as a side effect.
       is_mstd2d((char*)&i, R, C, (char*)scratch, (char*)(scratch+4));
 
-      do_stack(tid, R, C, N, i, sum, diff, N/3);
-#pragma omp critical      
+      do_stack2d(tid, R, C, N, i, sum, diff, N/3);
+#pragma omp critical
       printf("tid %d ending mask %lld\n", tid, i);
     }
 #pragma omp critical
@@ -292,21 +444,45 @@ void parallel_stack_search(int R, int C) {
 }
 
 
-int main(int argc, char** argv) {
+void oned(int argc, char** argv) {
+  if (argc < 2) {
+    printf("need N on the command line.\n");
+    exit(1);
+  }
+  int N = atoi(argv[1]);
+  if (N > 63) {
+    printf("N must be no more than 63. (got %d)\n",  N);
+    exit(1);
+  }
+
+  //parallel_exhaustive_search1d(N);
+  //stack_search1d(N);
+  parallel_stack_search1d(N);
+
+}
+
+void twod(int argc, char** argv) {
   if (argc < 3) {
     printf("need R and C on the command line.\n");
-    return 1;
+    exit(1);
   }
   int R = atoi(argv[1]);
   int C = atoi(argv[2]);
   if (R * C > 63) {
     printf("R * C must be no more than 63. (got %d)\n",  R * C);
-    return 1;
+    exit(1);
   }
 
-  //parallel_exhaustive_search(R, C);
-  //stack_search(R, C);
-  parallel_stack_search(R, C);
+  //parallel_exhaustive_search2d(R, C);
+  //stack_search2d(R, C);
+  parallel_stack_search2d(R, C);
+
+}
+
+
+int main(int argc, char** argv) {
+  //twod(argc, argv);
+  oned(argc, argv);
 
   return 0;
 }
